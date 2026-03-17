dfd0f88de6121c7434d3791c3f4316cc84c00230
Steven Algoet <info@telenut.be>
2026-03-17 11:40:35 +0100

    Added flow triggers for power and daily yield, fixed app.json validation

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-'use strict';
-
 const Homey = require('homey');
 
-module.exports = class MyApp extends Homey.App {
-
-  /**
-   * onInit is called when the app is initialized.
-   */
+class ElekeeperApp extends Homey.App {
   async onInit() {
-    this.log('MyApp has been initialized');
+    this.log('Elekeeper App is initializing...');
   }
 
-};
+  // Functie om de trigger voor vermogen op te halen en af te vuren
+  async triggerPowerChanged(device, tokens) {
+    const trigger = this.homey.flow.getDeviceTriggerCard('power_changed');
+    if (trigger) {
+      return trigger.trigger(device, tokens);
+    }
+  }
+
+  // Functie om de trigger voor dagopbrengst op te halen en af te vuren
+  async triggerTodayYieldChanged(device, tokens) {
+    const trigger = this.homey.flow.getDeviceTriggerCard('today_yield_changed');
+    if (trigger) {
+      return trigger.trigger(device, tokens);
+    }
+  }
+}
+
+module.exports = ElekeeperApp;
